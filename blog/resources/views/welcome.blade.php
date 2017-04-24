2fcd61ffcc5df4348ebc5592108881277e8df298
Mustafa Buda <mustafabuda@gmail.com>
2017-04-24 18:08:04 +0300

    Episode 5
    https://laracasts.com/series/laravel-from-scratch-2017/episodes/5

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <title></title>
 
     </head>
     <body>
-        <h1>Hello World</h1>
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task }}</li>
+            @endforeach
+        </ul>
     </body>
 </html>
